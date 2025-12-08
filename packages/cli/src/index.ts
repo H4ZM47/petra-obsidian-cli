30fcd1bd627c99177d1ef8f4a87f7e0965ec1fe7
alex-haslach <haslach@gmail.com>
2025-12-08 12:32:49 -0600

    feat: scaffold CLI with commander and command structure

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,2 +1,19 @@
 #!/usr/bin/env node
-console.log("petra CLI");
+
+import { Command } from "commander";
+import { VERSION } from "@petra/shared";
+import { registerCommands } from "./commands/index.js";
+
+const program = new Command();
+
+program
+  .name("petra")
+  .description("CLI for Obsidian vaults")
+  .version(VERSION)
+  .option("-j, --json", "Output as JSON")
+  .option("-q, --quiet", "Suppress non-error output")
+  .option("-v, --vault <path>", "Override vault path");
+
+registerCommands(program);
+
+program.parse();
